f9dfbfb3185392c4fe720be88d3407a83335fe97
Aadarsh Senapati <57413192+aadarshsenapati@users.noreply.github.com>
2025-03-25 21:39:21 +0530

    Add files via upload

---
+++ expense_split/includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="bg-primary text-white text-center py-3 footer">
+<p>&copy; <?php echo date('Y');?> Expense Splitter | <a href="https://aadarshsenapati.in/" target="_blank"> Designed by Aadarsh Senapati </a></p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
